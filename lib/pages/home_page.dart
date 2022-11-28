0c78f868c02826daeabf62ad43d5a0440eb9c267
JENIFERALVAREZUTEA <202000202d@utea.edu.pe>
2022-11-28 15:40:30 -0500

    video 16 segundo commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+
+  getdataPokemon()async{
+    Uri _uri = Uri.parse("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+  http.Response response = await http.get(_uri);
+  print(response.statusCode);
+  print(response.body);
+  
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("data"),
+          onPressed: (){
+           getdataPokemon();
+          },
+        ),
+      ),
+    );
   }
 }
